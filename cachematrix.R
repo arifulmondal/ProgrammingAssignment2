a8d52c87e18724811efd7f9a947568dacb6b46c6
arifulmondal <ariful.mondal@gmail.com>
2014-08-23 23:59:00 -0700

    Tested and fixed bugs, added descriptions

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,11 @@
-## Put comments here that give an overall description of what your
+## Program Assignment 2
+## rprog-006 - Coursera
+## Author: Ariful Mondal
+## August 2014
+##-------------------------------------------------------
+
+## cachematrix.R
+## 
 ## functions do
 
 ## Write a short comment describing this function
@@ -26,11 +33,34 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         minv <- x$getinv()
         if(!is.null(minv)) {
-                message("getting cached data")
+                message("getting inverse from cached data")
                 return(minv)
         }
         data <- x$get()
         minv <- solve(data, ...)
-        x$setmean(minv)
+        x$setinv(minv)
         minv
 }
+
+
+##----------------------------------------------##
+## How to use cacheSolve and  makeCacheMatrix
+
+##myMat<-matrix(rnorm(16), 4)
+##myMat<- matrix(c(1,0,0,2),2)
+## Caching matrix
+##chMat = makeCacheMatrix(myMat)
+## Calculte Inverse and put in Cache
+##chMat$get()
+## Run1
+##cacheSolve(chMat)
+
+## Run 2
+##myInvMat<-cacheSolve(chMat)
+##myInvMat
+##  Check
+##myInvMat%*%myMat
+##----------------------------------------------##
+
+
+
